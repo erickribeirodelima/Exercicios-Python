2f55c683bec3f85df71af3d123e218cff343f280
Erick Ribeiro <93787070+erickribeirodelima@users.noreply.github.com>
2022-04-10 18:08:38 -0300

    Exercício 1

---
+++ 01_nome.py
@@ -0,0 +1,8 @@
+"""
+
+01 - Escreva um programa que pergunte seu nome e depois mostre na tela uma saudação.
+
+"""
+
+nome = input("Olá, qual é o seu nome? ").strip().title()
+print(f"Olá {nome}, seja bem vindo(a) ao nosso sistema.")
